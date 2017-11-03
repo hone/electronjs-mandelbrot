11e6c73303a2209e0e74e38a82077d33cce2f9fa
Terence Lee <hone02@gmail.com>
2017-11-03 18:25:08 -0500

    add rust drawing

--- render.js
+++ render.js
@@ -1,3 +1,5 @@
+const rust = require('neon-mandelbrot')
+
 function checkIfBelongsToMandelbrotSet(x, y, maxIterations) {
   var realComponentOfResult = x;
   var imaginaryComponentOfResult = y;
@@ -38,6 +40,28 @@ function generate_mandelbrot(width, height, maxIterations, magnification, panX, 
   return image;
 }
 
+function drawRust(maxIterations) {
+  // Create Canvas
+  var myCanvas = document.getElementById("screen");
+  var ctx = myCanvas.getContext("2d");
+
+  var image = new Uint8Array(rust.rust_mandelbrot());
+  for(var x=0; x < myCanvas.width; x++) {
+    for(var y=0; y < myCanvas.height; y++) {
+      var index = x * myCanvas.width + y;
+      var belongsToSet = image[index];
+      if(belongsToSet == 0) {
+        ctx.fillStyle = '#000';
+        ctx.fillRect(x,y, 1,1); // Draw a black pixel
+      } else {
+        ctx.fillStyle = '#FFF';
+        //ctx.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
+        ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
+      }
+    }
+  }
+}
+
 function draw(maxIterations) {
   // Create Canvas
   var myCanvas = document.getElementById("screen");
@@ -68,8 +92,13 @@ function draw(maxIterations) {
   window.onload = function() {
     document.getElementById("render").onclick = function fun() {
       var maxIterations = parseInt(document.getElementById("iterations").value);
+      var backend = document.getElementById("backend").selectedOptions[0].value;
       var t1 = Date.now();
-      draw(maxIterations);
+      if (backend == "javascript") {
+        draw(maxIterations);
+      } else {
+        drawRust(maxIterations);
+      }
       var t2 = Date.now();
       var render_time = t2 - t1;
       document.getElementById("render_time").innerText = `Time: ${render_time.toFixed(2)}ms`;
