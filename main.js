0d5441dd7d08d3b44a92eb770e0fad86037de154
Terence Lee <hone02@gmail.com>
2017-11-04 20:50:34 -0500

    add ability to zoom & pan

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 630, height: 675})
+  win = new BrowserWindow({width: 630, height: 750})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
--- render.js
+++ render.js
@@ -4,15 +4,9 @@ const rust_parallel = {
 };
 const js = require('./mandelbrot.js');
 
-function draw(backend, maxIterations) {
-  // Create Canvas
+function draw(backend, maxIterations, magnificationFactor, panX, panY) {
   var myCanvas = document.getElementById("screen");
   var ctx = myCanvas.getContext("2d");
-
-  // Start drawing
-  var magnificationFactor = 200;
-  var panX = 2;
-  var panY = 1.5;
 
   var image = backend.generate(myCanvas.width, myCanvas.height, maxIterations, magnificationFactor, panX, panY);
   for (var x=0; x < myCanvas.width; x++) {
@@ -34,6 +28,9 @@ function draw(backend, maxIterations) {
   document.getElementById("render").onclick = function fun() {
     var maxIterations = parseInt(document.getElementById("iterations").value);
     var backend_value = document.getElementById("backend").selectedOptions[0].value;
+    var magnification = parseFloat(document.getElementById("magnification").value);
+    var panX = parseFloat(document.getElementById("panX").value);
+    var panY = parseFloat(document.getElementById("panY").value);
     var backend;
     var t1 = Date.now();
     switch (backend_value) {
@@ -47,7 +44,7 @@ function draw(backend, maxIterations) {
         backend = rust_parallel;
         break;
     }
-    draw(backend, maxIterations);
+    draw(backend, maxIterations, magnification, panX, panY);
     var t2 = Date.now();
     var render_time = t2 - t1;
     document.getElementById("render_time").innerText = `Time: ${render_time.toFixed(2)}ms`;
